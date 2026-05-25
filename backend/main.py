a66f54d2d401931b5a310fe22b80f8e16708ffc5
Naman Kumar <namankumar1170@gmail.com>
2026-05-25 14:41:54 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,65 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
-import pandas as pd
 
 app = FastAPI()
-
-model = joblib.load(r"C:\Users\DELL\OneDrive\Desktop\fintech fraud detection\models\fraud_model.pkl")
-
-class Transaction(BaseModel):
-    Time: float
-    V1: float
-    V2: float
-    V3: float
-    Amount: float
-
 @app.get("/")
 def home():
-    return {"message": "Fraud Detection API Running"}
+    return {"message": "Backend Running"}
 
 @app.post("/predict")
-def predict(data: Transaction):
-
-    input_data = pd.DataFrame([data.dict()])
-
-    prediction = model.predict(input_data)[0]
-
-    result = "Fraud" if prediction == 1 else "Not Fraud"
-
+def predict(data: dict):
     return {
-        "prediction": result
+        "prediction": "Fraud",
+        "recieved_data": data
     }
-from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
-import pandas as pd
-
-app = FastAPI()
-
-model = joblib.load(r"C:\Users\DELL\OneDrive\Desktop\fintech fraud detection\models\fraud_model.pkl")
-
-class Transaction(BaseModel):
-    Time: float
-    V1: float
-    V2: float
-    V3: float
-    Amount: float
-
-@app.get("/")
-def home():
-    return {"message": "Fraud Detection API Running"}
-
-@app.post("/predict")
-def predict(data: Transaction):
-
-    input_data = pd.DataFrame([data.dict()])
-
-    prediction = model.predict(input_data)[0]
-
-    result = "Fraud" if prediction == 1 else "Not Fraud"
-
-    return {
-        "prediction": result
-    }
-

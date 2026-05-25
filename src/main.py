879b451b952b45358ae989bf6547c9d674eb22f3
Naman Kumar <namankumar1170@gmail.com>
2026-05-25 14:44:06 +0530

    commit

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ Import all the required libraries
 """
 
 import numpy as np
-import pandas as pd
+import pandas as pd  
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
